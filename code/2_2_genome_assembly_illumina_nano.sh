4bd5e1ac2ba99bd222e2afde1c5ec3ffcd6483f4
rpensch <rar.pensch@gmail.com>
2020-04-03 14:36:32 +0200

    spades assembly 1 update

--- code/2_2_genome_assembly_illumina_nano.sh
+++ code/2_2_genome_assembly_illumina_nano.sh
@@ -16,6 +16,6 @@ module load spades
 spades.py --isolate \
 -1 data/genomic_data/raw_data/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz \
 -2 data/genomic_data/raw_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz \
---nanopore ../data/genomic_data/raw_data/Nanopore/E745_all.fasta.gz \
+--nanopore data/genomic_data/raw_data/Nanopore/E745_all.fasta.gz \
 -k 55 -o analyses/2_genome_assembly
 
