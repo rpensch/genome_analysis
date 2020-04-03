cd69ba7a875b53ad86c360ee91b9d95c7b2c4f40
rpensch <rar.pensch@gmail.com>
2020-04-03 13:45:50 +0200

    gather transcriptomic data

--- code/0_gather_data.sh
+++ code/0_gather_data.sh
@@ -1,7 +1,6 @@
 #Gathering the data in the working directory by creating soft links
 
-#Genomic data
-
+#Genomics data
 ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/genomics_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.1.subreads.fastq.gz ../data/raw_data/genomic_data/PacBio
 ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/genomics_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.2.subreads.fastq.gz ../data/raw_data/genomic_data/PacBio
 ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/genomics_data/PacBio/m131023_233432_42174_c100519312550000001823081209281335_s1_X0.3.subreads.fastq.gz ../data/raw_data/genomic_data/PacBio
@@ -13,3 +12,6 @@ ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/genomics_data/Illumina/E745-1
 ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/genomics_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz ../data/raw_data/genomic_data/Illumina
 
 ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/genomics_data/Nanopore/E745_all.fasta.gz ../data/genomic_data/raw_data/Nanopore
+
+#Transcriptomics data
+ln -s /proj/g2020008/nobackup/private/1_Zhang_2017/transcriptomics_data/ ../data/
