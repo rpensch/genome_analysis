e98ef8ae4f178cb29b95cd9153be7b5746ac5ea6
rpensch <rar.pensch@gmail.com>
2020-04-06 20:32:03 +0200

    mummerplot

--- code/3_2_assembly_evaluation_mummerplot.sh
+++ code/3_2_assembly_evaluation_mummerplot.sh
@@ -18,5 +18,6 @@ analyses/2_genome_assembly/2_efaecium_spades/efaecium_spades.contigs.fasta \
 
 mummerplot -c -p 1_assembly_mummerplot -S 1_assembly_nucmer.delta
 mummerplot -c -p 2_assembly_mummerplot 2_assembly_nucmer.delta
-
+mummerplot -c -p 3_assembly_mummerplot -S -R -Q 1_assembly_nucmer.delta
+mummerplot -c -p 4_assembly_mummerplot -R -Q 2_assembly_nucmer.delta
 
