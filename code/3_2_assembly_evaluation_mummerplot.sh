5c1bb5d0bfe7a4f5100d20457afb58cfecec4837
rpensch <rar.pensch@gmail.com>
2020-04-07 12:07:42 +0200

    mumerplot

--- code/3_2_assembly_evaluation_mummerplot.sh
+++ code/3_2_assembly_evaluation_mummerplot.sh
@@ -1,23 +1,14 @@
-#!/bin/bash -l   
-#SBATCH -A g2020008 
-#SBATCH -p core 
-#SBATCH -n 2 
-#SBATCH -t 02:00:00 
-#SBATCH -J mummerplot 
-#SBATCH --mail-type=ALL 
-#SBATCH --mail-user rar.pensch@gmail.com  
- 
-# Load modules 
+
 module load bioinfo-tools 
 module load MUMmer
 
-# Your commands
 nucmer --mum -p 1_assembly_nucmer \  
 analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
 analyses/2_genome_assembly/2_efaecium_spades/efaecium_spades.contigs.fasta
 
-mummerplot -c -p 1_assembly_mummerplot -S 1_assembly_nucmer.delta
-mummerplot -c -p 2_assembly_mummerplot 1_assembly_nucmer.delta 
-#mummerplot -c -p 3_assembly_mummerplot -S -R -Q 1_assembly_nucmer.delta
-#mummerplot -c -p 4_assembly_mummerplot -R -Q 1_assembly_nucmer.delta
+mummerplot -c -p 5_assembly_mummerplot 1_assembly_mummerplot_SNP_cov -S --png 1_assembly_nucmer.delta
+mummerplot -c -p 6_assembly_mummerplot 1_assembly_mummerplot_cov --png 1_assembly_nucmer.delta 
+mummerplot -p 7_assembly_mummerplot 1_assembly_mummerplot_SNP -S --png 1_assembly_nucmer.delta
+mummerplot -p 8_assembly_mummerplot 1_assembly_mummerplot --png 1_assembly_nucmer.delta 
 
+#Q: what about -R and -Q (student manual)?
