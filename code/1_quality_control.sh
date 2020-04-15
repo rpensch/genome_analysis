3e07dacf59f8021d6c444603d92440fb3f7a5539
rpensch <rar.pensch@gmail.com>
2020-04-15 14:10:08 +0200

    fastqc code update

--- code/1_quality_control.sh
+++ code/1_quality_control.sh
@@ -2,4 +2,4 @@
 
 #Illumina sequences with FastQC
 
-fastqc ../genomic_data/raw_data/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz ../genomic_data/raw_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz --outdir=../analyses/1_preprocessing/fastqc_raw
+fastqc data/genomic_data/raw_data/Illumina/E745-1.L500_SZAXPI015146-56_1_clean.fq.gz data/genomic_data/raw_data/Illumina/E745-1.L500_SZAXPI015146-56_2_clean.fq.gz --outdir=analyses/1_preprocessing/fastqc_raw
