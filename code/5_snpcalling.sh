25a3fdb22b951f0dae2e522589432e398a03ca7c
rpensch <rar.pensch@gmail.com>
2020-04-28 08:33:43 +0200

    changes

--- code/5_snpcalling.sh
+++ code/5_snpcalling.sh
@@ -1,7 +1,25 @@
-
-
 module load bioinfo-tools
 module load bcftools
 module load python/2.7.15
+module load bwa
 
-bcftools
+
+bwa index reference_genome -p efaecalis_index -a is
+bwa aln reference_genome reads > my.sai
+bwa samse reference_genome my.sai reads > my.sam
+
+#https://www.ebi.ac.uk/sites/ebi.ac.uk/files/content.ebi.ac.uk/materials/2014/140217_AgriOmics/dan_bolser_snp_calling.pdf
+
+bwa index ref.fa
+
+bwa mem ref.fa reads.fq > aln-se.sam
+
+bwa mem ref.fa read1.fq read2.fq > aln-pe.sam
+
+bwa aln ref.fa short_read.fq > aln_sa.sai
+
+bwa samse ref.fa aln_sa.sai short_read.fq > aln-se.sam
+
+bwa sampe ref.fa aln_sa1.sai aln_sa2.sai read1.fq read2.fq > aln-pe.sam
+
+bwa bwasw ref.fa long_read.fq > aln.sam 
