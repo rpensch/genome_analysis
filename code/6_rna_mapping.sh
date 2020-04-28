4e5d9e685a9011a99384ac3af1797b07ed27d7ef
rpensch <rar.pensch@gmail.com>
2020-04-28 09:49:06 +0200

    rna mapping code

--- code/6_rna_mapping.sh
+++ code/6_rna_mapping.sh
@@ -1,19 +1,40 @@
+#!/bin/bash -l   
+#SBATCH -A g2020008 
+#SBATCH -p core 
+#SBATCH -n 2 
+#SBATCH -t 06:00:00 
+#SBATCH -J rna_mapping 
+#SBATCH --mail-type=ALL 
+#SBATCH --mail-user rar.pensch@gmail.com  
+
+# Load modules 
 module load bioinfo-tools
 module load bwa
 
+# Your commands
 bwa index analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta
 
+bwa mem analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/transcriptomics_data/RNA-Seq_BH/trim_paired_ERR1797972_pass_1.fastq.gz \
+data/transcriptomics_data/RNA-Seq_BH/trim_paired_ERR1797972_pass_2.fastq.gz > analyses/6_rna_mapping/aln_BH_paired_ERR1797972.pe.sam
 
-bwa index ref.fa
+bwa mem analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/transcriptomics_data/RNA-Seq_BH/trim_paired_ERR1797974_pass_1.fastq.gz \
+data/transcriptomics_data/RNA-Seq_BH/trim_paired_ERR1797974_pass_2.fastq.gz > analyses/6_rna_mapping/aln_BH_paired_ERR1797974.pe.sam
 
-bwa mem ref.fa reads.fq > aln-se.sam
+bwa mem analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/transcriptomics_data/RNA-Seq_BH/trim_paired_ERR1797973_pass_1.fastq.gz \
+data/transcriptomics_data/RNA-Seq_BH/trim_paired_ERR1797973_pass_2.fastq.gz > analyses/6_rna_mapping/aln_BH_paired_ERR1797973.pe.sam
 
-bwa mem ref.fa read1.fq read2.fq > aln-pe.sam
+bwa mem analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797969_pass_1.fastq.gz \
+data/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797969_pass_2.fastq.gz > analyses/6_rna_mapping/aln_Serum_paired_ERR1797969.pe.sam
 
-bwa aln ref.fa short_read.fq > aln_sa.sai
+bwa mem analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797970_pass_1.fastq.gz \
+data/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797970_pass_2.fastq.gz > analyses/6_rna_mapping/aln_Serum_paired_ERR1797970.pe.sam
 
-bwa samse ref.fa aln_sa.sai short_read.fq > aln-se.sam
+bwa mem analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797971_pass_1.fastq.gz \
+data/transcriptomics_data/RNA-Seq_Serum/trim_paired_ERR1797971_pass_2.fastq.gz > analyses/6_rna_mapping/aln_Serum_paired_ERR1797971.pe.sam
 
-bwa sampe ref.fa aln_sa1.sai aln_sa2.sai read1.fq read2.fq > aln-pe.sam
-
-bwa bwasw ref.fa long_read.fq > aln.sam 
