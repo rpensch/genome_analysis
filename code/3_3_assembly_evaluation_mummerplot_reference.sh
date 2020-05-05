532f92f68a9d3e98c9bb428cfa7fc3e05ded3c83
rpensch <rar.pensch@gmail.com>
2020-05-05 14:11:19 +0200

    mummerplot canu reference

---
+++ code/3_3_assembly_evaluation_mummerplot_reference.sh
@@ -0,0 +1,11 @@
+
+module load bioinfo-tools 
+module load MUMmer
+
+nucmer --mum -p 1_assembly_reference \  
+analyses/2_genome_assembly/1_efaecium_canu/efaecium_canu.contigs.fasta \
+data/reference/GCF_001720945.1_ASM172094v1_genomic.fna
+
+mummerplot -p 1_assembly_mummerplot_reference --png 1_assembly_reference.delta 
+
+#Q: what about -R and -Q (student manual)?
