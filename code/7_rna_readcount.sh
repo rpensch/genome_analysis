8d8f93e329d5911887930b4a02a1f53399874a37
rpensch <rar.pensch@gmail.com>
2020-05-05 13:47:22 +0200

    update code rna read count

--- code/7_rna_readcount.sh
+++ code/7_rna_readcount.sh
@@ -13,14 +13,14 @@ module load htseq
 
 # Your commands
 
-htseq-count -f bam -r pos -t CDS analyses/6_rna_mapping/rna_mapping_BH_paired_ERR1797972.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_BH_paired_ERR1797972.txt
+htseq-count -f bam -r pos -t CDS -i locus_tag analyses/6_rna_mapping/rna_mapping_BH_paired_ERR1797972.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_BH_paired_ERR1797972.txt
 
-htseq-count -f bam -r pos -t CDS analyses/6_rna_mapping/rna_mapping_BH_paired_ERR1797974.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_BH_paired_ERR1797974.txt
+htseq-count -f bam -r pos -t CDS -i locus_tag analyses/6_rna_mapping/rna_mapping_BH_paired_ERR1797974.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_BH_paired_ERR1797974.txt
 
-htseq-count -f bam -r pos -t CDS analyses/6_rna_mapping/rna_mapping_BH_paired_ERR1797973.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_BH_paired_ERR1797973.txt
+htseq-count -f bam -r pos -t CDS -i locus_tag analyses/6_rna_mapping/rna_mapping_BH_paired_ERR1797973.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_BH_paired_ERR1797973.txt
 
-htseq-count -f bam -r pos -t CDS analyses/6_rna_mapping/rna_mapping_Serum_paired_ERR1797969.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_Serum_paired_ERR1797969.txt
+htseq-count -f bam -r pos -t CDS -i locus_tag analyses/6_rna_mapping/rna_mapping_Serum_paired_ERR1797969.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_Serum_paired_ERR1797969.txt
 
-htseq-count -f bam -r pos -t CDS analyses/6_rna_mapping/rna_mapping_Serum_paired_ERR1797970.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_Serum_paired_ERR1797970.txt
+htseq-count -f bam -r pos -t CDS -i locus_tag analyses/6_rna_mapping/rna_mapping_Serum_paired_ERR1797970.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_Serum_paired_ERR1797970.txt
 
-htseq-count -f bam -r pos -t CDS analyses/6_rna_mapping/rna_mapping_Serum_paired_ERR1797971.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_Serum_paired_ERR1797971.txt
+htseq-count -f bam -r pos -t CDS -i locus_tag analyses/6_rna_mapping/rna_mapping_Serum_paired_ERR1797971.bam analyses/4_annotation/efaecium_annotation_noseq.gff > analyses/7_rna_readcount/htseq_readcount_Serum_paired_ERR1797971.txt
